dd766b0fa0f223f02d27cab4a6810db86da80d15
sharmila <sharmilajoshi19@gmail.com>
2021-12-30 16:25:44 +0530

    ingested data in event hub

--- src/main/java/com/harman/LatestEventhubJsonIngetionApplication.java
+++ src/main/java/com/harman/LatestEventhubJsonIngetionApplication.java
@@ -19,6 +19,7 @@ public class LatestEventhubJsonIngetionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LatestEventhubJsonIngetionApplication.class, args);
+		publishEvents();
 	}
 	
 
