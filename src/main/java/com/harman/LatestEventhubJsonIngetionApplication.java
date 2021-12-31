e98c30735308a16c28e95a453d4a977bcfef031f
SharmilaJN <43439142+SharmilaJN@users.noreply.github.com>
2021-12-31 13:31:36 +0530

    updated string values on friday

--- src/main/java/com/harman/LatestEventhubJsonIngetionApplication.java
+++ src/main/java/com/harman/LatestEventhubJsonIngetionApplication.java
@@ -45,12 +45,12 @@ public class LatestEventhubJsonIngetionApplication {
 
         // sample events in an array
         CellularInfo cell = new CellularInfo();
-        cell.setCity("punenew1111json");
-        cell.setPhNo("12345678");
+        cell.setCity("punenew1111jsonfriday");
+        cell.setPhNo("12");
         
         CellularInfo cell1 = new CellularInfo();
-        cell.setCity("delhi1111json");
-        cell1.setPhNo("9999999");
+        cell1.setCity("delhi1111jsonfriday");
+        cell1.setPhNo("99");
         
        // System.out.println(cell.toJSON() + cell1.toJSON());
 	//List<EventData> allEvents = Arrays.asList(new EventData("test1111"), new EventData("message111"));
